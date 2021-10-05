bc4c061155c2063d5b9d2cffc388d8458c05ec7c
Kenny Fournie <FourKenn744@GoTVDSB.ca>
2021-10-05 13:11:36 -0400

    finished prompter

--- Program.cs
+++ Program.cs
@@ -15,8 +15,27 @@ namespace part_6___loops
             int max;
             int input;
 
+            Console.WriteLine("enter a minimum number");
+            min = Convert.ToInt32(Console.ReadLine());
 
+            Console.WriteLine("enter a maximum number");
+            max = Convert.ToInt32(Console.ReadLine());
 
+            Console.WriteLine("now enter a number between the two");
+            input = Convert.ToInt32(Console.ReadLine());
+
+            while ((input <= min) || (input >= max))
+            {
+                Console.WriteLine("enter a number between the two");
+                input = Convert.ToInt32(Console.ReadLine());
+            }
+
+            if((input > min) && (input < max))
+            {
+                Console.WriteLine("you entered " + input + " which is in between " + min + " and " + max);
+            }
+
+            Console.ReadLine();
         }
     }
 }
