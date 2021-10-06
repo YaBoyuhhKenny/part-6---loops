5c4757d25e453504bf1f585eb7fb80a20fec6cd8
Kenny Fournie <FourKenn744@GoTVDSB.ca>
2021-10-06 13:27:38 -0400

    percent passing completed

--- Program.cs
+++ Program.cs
@@ -6,74 +6,98 @@ using System.Threading.Tasks;
 
 namespace part_6___loops
 {
+    
     class Program
     {
         static void Main(string[] args)
         {
-            ////prompter
-            //int min;
-            //int max;
-            //int input;
+            //prompter
+            int min;
+            int max;
+            int input;
 
-            //Console.WriteLine("enter a minimum number");
-            //min = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("enter a minimum number");
+            min = Convert.ToInt32(Console.ReadLine());
 
-            //Console.WriteLine("enter a maximum number");
-            //max = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("enter a maximum number");
+            max = Convert.ToInt32(Console.ReadLine());
 
-            //Console.WriteLine("now enter a number between the two");
-            //input = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("now enter a number between the two");
+            input = Convert.ToInt32(Console.ReadLine());
 
-            //while ((input <= min) || (input >= max))
-            //{
-            //    Console.WriteLine("enter a number between the two");
-            //    input = Convert.ToInt32(Console.ReadLine());
-            //}
+            while ((input <= min) || (input >= max))
+            {
+                Console.WriteLine("enter a number between the two");
+                input = Convert.ToInt32(Console.ReadLine());
+            }
 
-            //if ((input > min) && (input < max))
-            //{
-            //    Console.WriteLine("you entered " + input + " which is in between " + min + " and " + max);
-            //}
+            if ((input > min) && (input < max))
+            {
+                Console.WriteLine("you entered " + input + " which is in between " + min + " and " + max);
+            }
 
-            ////PercentPassing
+            Console.WriteLine();
 
-            //bool done = false;
-            //int totalPass = 0, percent;
-            //Console.WriteLine("enter the grade percentages and follow instructions to end the program");
-            //while (done != true)
-            //{
-            //    percent = Convert.ToInt32(Console.ReadLine());
-            //    if (percent > 70)
-            //    {
-            //        totalPass = totalPass + 1;
-            //    }
+            //PercentPassing
 
-            //    Console.WriteLine("do you want to continue? Y or N");
-            //    string answer = Console.ReadLine().ToUpper();
-            //    if (answer == "N")
-            //    {
-            //        done = true;
-            //    }
-            //}
+            bool done = false;
+            int totalPass = 0, percent;
+            Console.WriteLine("enter the grade percentages and follow instructions to end the program");
+            while (done != true)
+            {
+                percent = Convert.ToInt32(Console.ReadLine());
+                if (percent > 70)
+                {
+                    totalPass = totalPass + 1;
+                }
 
-            //Console.WriteLine($"the number of passing grades are {totalPass}");
+                Console.WriteLine("do you want to continue? Y or N");
+                string answer = Console.ReadLine().ToUpper();
+                if (answer == "N")
+                {
+                    done = true;
+                }
+            }
 
-            ////oddSum
+            Console.WriteLine($"the number of passing grades are {totalPass}");
 
-            //int userInput;
+            Console.WriteLine();
 
-            //Console.WriteLine("enter a number to get the odd numbers in it");
-            //userInput = Convert.ToInt32(Console.ReadLine());
+            //oddSum
 
-            //for (int i = 1; i < userInput; i = i + 2)
-            //{
-            //    Console.WriteLine(Convert.ToString(i));
-            //}
+            int userInput;
 
+            Console.WriteLine("enter a number to get the odd numbers in it");
+            userInput = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 1; i < userInput; i = i + 2)
+            {
+                Console.WriteLine(Convert.ToString(i));
+            }
+
+            Console.WriteLine();
 
             //randomNumbers
 
+            int[] numbers = new int[25];
+            Random rnd = new Random();
+            
 
+
+            Console.WriteLine("enter a minimum number");
+            min = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("enter a maximum number");
+            max = Convert.ToInt32(Console.ReadLine());
+
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                numbers[i] = rnd.Next(min, max);
+                Console.WriteLine(numbers[i]);
+            }
+
+            Console.WriteLine("press anything to close");
 
 
             Console.ReadLine();
