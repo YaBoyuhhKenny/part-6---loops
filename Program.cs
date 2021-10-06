7951b02aea9429bf591497d9c210c59a43a081de
Kenny Fournie <FourKenn744@GoTVDSB.ca>
2021-10-06 13:00:55 -0400

    made oddSum

--- Program.cs
+++ Program.cs
@@ -35,28 +35,42 @@ namespace part_6___loops
             //    Console.WriteLine("you entered " + input + " which is in between " + min + " and " + max);
             //}
 
-            //PercentPassing
-            
-            bool done = false;
-            int totalPass = 0, percent;
-            Console.WriteLine("enter the grade percentages and follow instructions to end the program");
-            while(done != true)
-            {
-                percent = Convert.ToInt32(Console.ReadLine());
-                if (percent > 50)
-                {
-                    totalPass = totalPass + 1; 
-                }
+            ////PercentPassing
 
-                Console.WriteLine("do you want to continue? Y or N");
-                string answer = Console.ReadLine().ToUpper();
-                if(answer == "N")
-                {
-                    done = true;
-                }
+            //bool done = false;
+            //int totalPass = 0, percent;
+            //Console.WriteLine("enter the grade percentages and follow instructions to end the program");
+            //while (done != true)
+            //{
+            //    percent = Convert.ToInt32(Console.ReadLine());
+            //    if (percent > 50)
+            //    {
+            //        totalPass = totalPass + 1;
+            //    }
+
+            //    Console.WriteLine("do you want to continue? Y or N");
+            //    string answer = Console.ReadLine().ToUpper();
+            //    if (answer == "N")
+            //    {
+            //        done = true;
+            //    }
+            //}
+
+            //Console.WriteLine($"the number of passing grades are {totalPass}");
+
+            //oddSum
+
+            int userInput;
+
+            Console.WriteLine("enter a number to get the odd numbers in it");
+            userInput = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 1; i < userInput; i = i + 2)
+            {
+                Console.WriteLine(Convert.ToString(i));
             }
 
-            Console.WriteLine($"the number of passing grades are {totalPass}");
+
 
 
 
